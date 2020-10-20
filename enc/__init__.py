7f7e89f88c775a56bb4a4a5be68f40b61842bded
Simon Blindheim <simon.blindheim@ntnu.no>
2020-10-20 14:36:28 +0200

    Hide supported object types from API

--- enc/__init__.py
+++ enc/__init__.py
@@ -1,5 +1,2 @@
 from .enc import Parser
-from .enc import supported_projection
-from .feature import supported_features
-from .region import supported_regions
 from .shapes import Area, Line, Position
