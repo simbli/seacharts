ef037d800826fba13d90f9435b583efc29714967
Simon Blindheim <simon.blindheim@ulmatec.no>
2023-10-30 20:19:21 +0100

    Change data path to use current working directory

--- seacharts/utils/paths.py
+++ seacharts/utils/paths.py
@@ -2,26 +2,26 @@ import pathlib
 
 # Defines (hard-coded paths to relevant files)
 root = pathlib.Path(__file__).parents[2]
-package = root / 'seacharts'
+package = root / "seacharts"
 
-config = package / 'config.yaml'
-config_schema = package / 'config_schema.yaml'
+config = package / "config.yaml"
+config_schema = package / "config_schema.yaml"
 
-data = root / 'data'
-external = data / 'external'
-shapefiles = data / 'shapefiles'
+data = pathlib.Path.cwd() / "data"
+external = data / "external"
+shapefiles = data / "shapefiles"
 
-vessels = data / 'vessels.csv'
+vessels = data / "vessels.csv"
 
-hazards = data / 'hazards'
-dynamic = hazards / 'dynamic.csv'
-static = hazards / 'static.csv'
+hazards = data / "hazards"
+dynamic = hazards / "dynamic.csv"
+static = hazards / "static.csv"
 
-paths = data / 'paths'
-path1 = paths / 'path1.csv'
-path2 = paths / 'path2.csv'
+paths = data / "paths"
+path1 = paths / "path1.csv"
+path2 = paths / "path2.csv"
 
-reports = root / 'reports'
-frames_dir = reports / 'frames'
-simulation = reports / 'simulation.gif'
-frame_files = frames_dir / 'frame_*.png'
+reports = root / "reports"
+frames_dir = reports / "frames"
+simulation = reports / "simulation.gif"
+frame_files = frames_dir / "frame_*.png"
