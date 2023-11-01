2f1a239c2362a1098d35015601139c66d65bffce
Simon Blindheim <simon.blindheim@ulmatec.no>
2023-11-01 15:18:27 +0100

    Refactor land, shore and seabed as properties

--- seacharts/enc.py
+++ seacharts/enc.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from seacharts.display import Display
 from seacharts.environment import Environment
+from seacharts.spatial.base import Layer
 from seacharts.utils.config import Config
 
 
@@ -21,32 +22,43 @@ class ENC:
         self._config = config if isinstance(config, Config) else Config(config)
         self._environment = Environment(self._config.settings)
         self._display = None
-        self.land = self._environment.topography.land
-        self.shore = self._environment.topography.shore
-        self.seabed = self._environment.hydrography.bathymetry
 
     @property
     def display(self) -> Display:
         """
-        :return: Display to visualize maritime geometric data and vessels
+        :return: display to visualize maritime geometric data and vessels
         """
         if self._display is None:
             self._display = Display(self._config.settings, self._environment)
         return self._display
 
     @property
-    def size(self) -> tuple[int, int]:
+    def land(self) -> Layer:
         """
-        :return: tuple of bounding box size
+        :return: land layer container of Shapely geometries
         """
-        return self._environment.scope.extent.size
+        return self._environment.topography.land
 
     @property
-    def center(self) -> tuple[int, int]:
+    def shore(self) -> Layer:
         """
-        :return: tuple of ENC center coordinates
+        :return: shore layer container of Shapely geometries
         """
-        return self._environment.scope.extent.center
+        return self._environment.topography.shore
+
+    @property
+    def seabed(self) -> dict[int, Layer]:
+        """
+        :return: seabed dict of Shapely geometries for each depth bin
+        """
+        return self._environment.hydrography.bathymetry
+
+    @property
+    def size(self) -> tuple[int, int]:
+        """
+        :return: tuple of ENC bounding box size
+        """
+        return self._environment.scope.extent.size
 
     @property
     def origin(self) -> tuple[int, int]:
@@ -54,6 +66,13 @@ class ENC:
         :return: tuple of ENC origin (lower left) coordinates.
         """
         return self._environment.scope.extent.origin
+
+    @property
+    def center(self) -> tuple[int, int]:
+        """
+        :return: tuple of ENC center coordinates
+        """
+        return self._environment.scope.extent.center
 
     @property
     def bbox(self) -> tuple[int, int, int, int]:
