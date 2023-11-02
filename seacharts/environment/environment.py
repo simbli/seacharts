459cbe0cd6456f4fbbed9a769c924749442c2ed2
Simon Blindheim <simon.blindheim@ulmatec.no>
2023-11-02 10:56:24 +0100

    Remove new_data setting and add ENC.update method

--- seacharts/environment/environment.py
+++ seacharts/environment/environment.py
@@ -1,18 +1,50 @@
 from __future__ import annotations
 
 import seacharts.spatial as spl
-from .extent import Extent
 from .scope import Scope
 
 
 class Environment:
-    supported_layers = ", ".join(spl.supported_layers)
-
     def __init__(self, settings: dict):
-        extent = Extent(settings)
-        self.scope = Scope(settings, extent)
+        self.scope = Scope(settings)
         self.hydrography = spl.Hydrography(self.scope)
         self.topography = spl.Topography(self.scope)
+        self.load_existing_shapefiles()
         self.safe_area = None
         self.ownship = None
         self.depth = None
+
+    def load_existing_shapefiles(self):
+        self.hydrography.load(self.scope)
+        self.topography.load(self.scope)
+        if self.hydrography.loaded or self.topography.loaded:
+            print("INFO: ENC created using data from existing shapefiles.\n")
+        else:
+            print("INFO: No existing spatial data was found.")
+            self.parse_data_into_shapefiles()
+
+    def parse_data_into_shapefiles(self):
+        if not list(self.scope.parser.gdb_paths):
+            resources = sorted(list(set(self.scope.resources)))
+            if not resources:
+                print(
+                    "WARNING: No spatial data source location given in config."
+                )
+            else:
+                message = "WARNING: No spatial data sources were located in\n"
+                message += "         "
+                resources = [f"'{r}'" for r in resources]
+                message += ", ".join(resources[:-1])
+                if len(resources) > 1:
+                    message += f" and {resources[-1]}"
+                print(message + ".")
+            return
+        print(
+            "INFO: Updating ENC with data from available resources..."
+        )
+        self.hydrography.parse(self.scope)
+        self.topography.parse(self.scope)
+        if self.hydrography.loaded or self.topography.loaded:
+            print("\nENC update complete.\n")
+        else:
+            print("WARNING: Given spatial data source(s) seem empty.\n")
