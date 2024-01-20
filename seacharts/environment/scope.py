e37a9a4b62aa6065f3e5b7a74cc8fe861af7463a
Simon Blindheim <simon.blindheim@ntnu.no>
2024-01-20 13:24:17 +0100

    Add parser package and move module from utils

--- seacharts/environment/scope.py
+++ seacharts/environment/scope.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import seacharts.utils as utils
+from seacharts.parser import ShapefileParser
 from .extent import Extent
 
 
@@ -12,7 +13,7 @@ class Scope:
         self.extent = Extent(settings)
         self.depths = settings["enc"].get("depths", self.default_depths)
         self.resources = settings["enc"].get("resources", [])
-        self.parser = utils.ShapefileParser(self.extent.bbox, self.resources)
+        self.parser = ShapefileParser(self.extent.bbox, self.resources)
         self.features = ["land", "shore"]
         for depth in self.depths:
             self.features.append(f"seabed{depth}m")
