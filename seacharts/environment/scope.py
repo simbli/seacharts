6759f87e58e1b8c2cf91c152329a82cab0901a00
Simon Blindheim <simon.blindheim@ntnu.no>
2024-01-21 12:02:41 +0100

    Rename ShapefileParser to DataParser

--- seacharts/environment/scope.py
+++ seacharts/environment/scope.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 import seacharts.utils as utils
-from seacharts.parser import ShapefileParser
+from seacharts.parser import DataParser
 from .extent import Extent
 
 
@@ -13,7 +13,7 @@ class Scope:
         self.extent = Extent(settings)
         self.depths = settings["enc"].get("depths", self.default_depths)
         self.resources = settings["enc"].get("resources", [])
-        self.parser = ShapefileParser(self.extent.bbox, self.resources)
+        self.parser = DataParser(self.extent.bbox, self.resources)
         self.features = ["land", "shore"]
         for depth in self.depths:
             self.features.append(f"seabed{depth}m")
