5404b0ab5ce36d519ecd80f225d3d66fa89c79b3
Simon Blindheim <simon.blindheim@ntnu.no>
2024-02-18 17:26:31 +0100

    Move parsing methods from Environment to parser

--- seacharts/spatial/spatial.py
+++ seacharts/spatial/spatial.py
@@ -1,5 +1,4 @@
-import time
-from dataclasses import dataclass, InitVar
+from dataclasses import dataclass
 
 from seacharts.environment.scope import Scope
 from seacharts.layers import Layer, Land, Shore, Seabed
@@ -7,12 +6,28 @@ from seacharts.layers import Layer, Land, Shore, Seabed
 
 @dataclass
 class SpatialData:
-    scope: InitVar[Scope]
+    scope: Scope
 
-    def __post_init__(self, scope: Scope):
-        self.bathymetry = {d: Seabed(d) for d in scope.depths}
+    def __post_init__(self):
+        self.bathymetry = {d: Seabed(d) for d in self.scope.depths}
         self.land = Land()
         self.shore = Shore()
+
+    def load_existing_shapefiles(self) -> None:
+        self.scope.parser.load_shapefiles(self.featured_layers)
+        if self.loaded:
+            print("INFO: ENC created using data from existing shapefiles.\n")
+        else:
+            print("INFO: No existing spatial data was found.\n")
+
+    def parse_resources_into_shapefiles(self) -> None:
+        self.scope.parser.parse_resources(
+            self.featured_layers, self.scope.resources, self.scope.extent.area
+        )
+        if self.loaded:
+            print("\nENC update complete.\n")
+        else:
+            print("WARNING: Given spatial data source(s) seem empty.\n")
 
     @property
     def layers(self) -> list[Layer]:
@@ -23,43 +38,9 @@ class SpatialData:
         return [layer for layer in self.layers if not layer.geometry.is_empty]
 
     @property
+    def featured_layers(self) -> list[Layer]:
+        return [x for x in self.layers if x.label in self.scope.features]
+
+    @property
     def loaded(self) -> bool:
         return any(self.loaded_layers)
-
-    def parse(self, scope: Scope) -> None:
-        layers = [x for x in self.layers if x.label in scope.features]
-        if not list(scope.parser.gdb_paths):
-            return
-        print(
-            f"\nProcessing {scope.extent.area // 10 ** 6} km^2 of ENC features:"
-        )
-        for layer in layers:
-            start_time = time.time()
-            records = scope.parser.load_fgdb(layer)
-            info = f"{len(records)} {layer.name} geometries"
-
-            if not records:
-                print(f"\rFound {info}.")
-                return
-            else:
-                print(f"\rMerging {info}...", end="")
-                layer.unify(records)
-
-                print(f"\rSimplifying {info}...", end="")
-                layer.simplify(0)
-
-                print(f"\rBuffering {info}...", end="")
-                layer.buffer(0)
-
-                print(f"\rClipping {info}...", end="")
-                layer.clip(scope.extent.bbox)
-
-            scope.parser.save(layer)
-            end_time = round(time.time() - start_time, 1)
-            print(f"\rSaved {info} to shapefile in {end_time} s.")
-
-    def load(self, scope: Scope) -> None:
-        layers = [x for x in self.layers if x.label in scope.features]
-        for layer in layers:
-            records = scope.parser.load_shapefile(layer)
-            layer.records_as_geometry(records)
