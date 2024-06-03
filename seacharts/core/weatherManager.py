532c9717fdf3925b7f7ae57130aff499686a7194
Konrad Drozd <konraddrozd02@gmail.com>
2024-06-03 15:33:18 +0200

    Update weatherManager.py

--- seacharts/core/weatherManager.py
+++ seacharts/core/weatherManager.py
@@ -1,18 +1,22 @@
 import requests
 
+
 class WeatherManager:
     def __init__(self, weatherSettings: dict):
-        query_dict = weatherSettings.copy()
+        self.unformatted_data = self.fetch_data(weatherSettings.copy())
+
+    @staticmethod
+    def fetch_data(query_dict):
         api_query = query_dict["Pythor_adress"] + "/api/weather?"
         query_dict.pop("Pythor_adress")
-        for k,v in query_dict.items():
+        for k, v in query_dict.items():
             api_query += k + "="
-            if not isinstance(v,list):
-                api_query += str(v)+"&"
+            if not isinstance(v, list):
+                api_query += str(v) + "&"
             else:
                 for weater_var in v:
-                    api_query+= str(weater_var) + ","
-                api_query = api_query[:-1]+"&"
+                    api_query += str(weater_var) + ","
+                api_query = api_query[:-1] + "&"
         api_query = api_query[:-1]
         print(api_query)
-        self.unformated_data = requests.get (api_query)
+        return requests.get(api_query)
