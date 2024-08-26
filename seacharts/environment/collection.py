929285279370019e41eff811febccbd20a26ea3e
miqn <mik.bra@wp.pl>
2024-08-26 19:39:49 +0200

    added extension for time in config to specify hours and minutes, minutes are not yet used anywhere

--- seacharts/environment/collection.py
+++ seacharts/environment/collection.py
@@ -3,7 +3,6 @@ Contains the DataCollection abstract class for containing parsed spatial data.
 """
 from abc import abstractmethod, ABC
 from dataclasses import dataclass, field
-
 from seacharts.core import Scope, DataParser
 from seacharts.layers import Layer
 
